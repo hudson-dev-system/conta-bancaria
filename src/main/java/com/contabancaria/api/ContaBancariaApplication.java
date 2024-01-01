63dd40b227ac095caff9d3ca5c28a2dddf4de1bc
hudson-dev-system <hudsondevsystem@gmail.com>
2024-01-01 06:15:22 -0300

    Inicio projeto se Deus quiser vai dar certo.

---
+++ src/main/java/com/contabancaria/api/ContaBancariaApplication.java
@@ -0,0 +1,13 @@
+package com.contabancaria.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaBancariaApplication.class, args);
+	}
+
+}
