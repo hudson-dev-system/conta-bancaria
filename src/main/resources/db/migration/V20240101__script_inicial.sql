b814406e94bb26c5ea290c6ca80a22300aafe017
hudson-dev-system <hudsondevsystem@gmail.com>
2024-01-02 02:03:20 -0300

    Add flyway.

---
+++ src/main/resources/db/migration/V20240101__script_inicial.sql
@@ -0,0 +1,32 @@
+CREATE TABLE agencia(
+    id INT NOT NULL,
+    cnpj VARCHAR(255) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+    data_cadastro DATETIME NOT NULL,
+    data_atualizacao DATETIME NOT NULL,
+    PRIMARY KEY(id)
+);
+CREATE TABLE correntista(
+    id INT NOT NULL,
+    agencia_id INT NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    cpf VARCHAR(255) NOT NULL,
+    cnpj VARCHAR(255) DEFAULT NULL,
+    email VARCHAR(255) NOT NULL,
+    tipo VARCHAR(255) NOT NULL,
+    saldo DECIMAL(19,2) NOT NULL,
+    data_cadastro DATETIME NOT NULL,
+    data_atualizacao DATETIME NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(agencia_id) REFERENCES agencia(id)
+);
+CREATE TABLE transacao(
+    id INT NOT NULL,
+    correntista_id INT NOT NULL,
+    tipo VARCHAR(255) NOT NULL,
+    data_cadastro DATETIME NOT NULL,
+    data_atualizacao DATETIME NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(correntista_id) REFERENCES correntista(id)
+);
